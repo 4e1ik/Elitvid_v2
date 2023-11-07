08a958d86964c5a0d1c9d1cb29cfd1cdfefe7f32
4e1ik <Artemi324@tut.by>
2023-11-07 18:45:31 +0300

    fix form v21

--- resources/views/layouts/elitvid/elitvid.blade.php
+++ resources/views/layouts/elitvid/elitvid.blade.php
@@ -236,7 +236,7 @@
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
 <script src="{{asset('/elitvid_assets/scripts/form.js')}}"></script>
-<script src="https://www.google.com/recaptcha/api.js"></script>
+{{--<script src="https://www.google.com/recaptcha/api.js"></script>--}}
 <script src="https://www.google.com/recaptcha/api.js?render={{config('services.recaptcha.site_key')}}"></script>
 @if(\Illuminate\Support\Facades\Route::currentRouteName() == 'home')
     <script src="{{asset('/elitvid_assets/scripts/swiper.js')}}"></script>
@@ -249,7 +249,7 @@
     function onClick(e) {
         e.preventDefault();
         grecaptcha.ready(function() {
-            grecaptcha.execute('{{config('services.recaptcha.site_key')}}', {action: 'submit'}).then(function(token) {
+            grecaptcha.execute('{{config('services.recaptcha.site_key')}}', {action: 'send_mail'}).then(function(token) {
                 document.getElementById('g-recaptcha-response').value = token;
                 document.getElementById('mail_form').submit();
             });
