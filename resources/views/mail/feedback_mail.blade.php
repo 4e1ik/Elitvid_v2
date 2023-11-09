0a5392b5dd5d9c3e57ce5477cfd7ae678d20debb
4e1ik <Artemi324@tut.by>
2023-11-09 21:53:34 +0300

    fix problems with mail v1.4

--- resources/views/mail/feedback_mail.blade.php
+++ resources/views/mail/feedback_mail.blade.php
@@ -13,10 +13,10 @@
             <h4>Имя пользователя: <span>{{$data['name']}}</span>.</h4>
             <h4>Почта пользователя: <span>{{$data['email']}}</span>.</h4>
             <h4>Телефон для связи пользователя: <span>{{$data['phone']}}</span>.</h4>
-            @if (!$data['name_corp'] == null)
+            @if (array_key_exists('name_corp', $data))
                 <h4>Название компании пользователя: <span>{{$data['name_corp']}}</span>.</h4>
             @endif
-            @if (!$data['textarea'] == null)
+            @if (array_key_exists('textarea', $data))
                 <h4>Комментарий пользователя: <span>{{$data['textarea']}}</span>.</h4>
             @endif
         </div>
