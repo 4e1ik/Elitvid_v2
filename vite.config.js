1989b8d669ef30ebc964abdc47896929a6caeed3
4e1ik <Artemi324@tut.by>
2023-12-20 15:16:00 +0300

    20.12.2023: upgrade project - add vite build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: 'public/elitvid_assets/style.scss',
             refresh: true,
         }),
         react(),
