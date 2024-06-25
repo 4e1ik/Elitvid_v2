543b2b2ff24090474b3b19add0d509a55c3302b8
4e1ik <Artemi324@tut.by>
2024-06-25 22:32:07 +0300

    fix pots pages 5

--- app/Http/Controllers/BenchProductController.php
+++ app/Http/Controllers/BenchProductController.php
@@ -58,7 +58,7 @@ class BenchProductController extends Controller
                 $path = Storage::putFileAs('images', $file, save_image($file, BenchImage::query())); // Даем путь к этому файлу
                 $data['image'] = $path;
 
-                ImageManager::gd()->read($file)->scaleDown(360,  290)->save(storage_path('app/images/'.save_image($file, BenchImage::query())));
+                ImageManager::gd()->read($file)->scaleDown(360,  290)->save(storage_path('app/public/images/'.save_image($file, BenchImage::query())));
 
                 BenchImage::create($data);
             }
@@ -120,7 +120,7 @@ class BenchProductController extends Controller
                 $data['image'] = $path;
                 BenchImage::create($data);
 
-                ImageManager::gd()->read($file)->scaleDown(360,  290)->save(storage_path('app/images/'.save_image($file, BenchImage::query())));
+                ImageManager::gd()->read($file)->scaleDown(360,  290)->save(storage_path('app/public/images/'.save_image($file, BenchImage::query())));
             }
         }
 
