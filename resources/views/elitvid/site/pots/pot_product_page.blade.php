e97da66b7b739de0e5d2d3ed3f16baa0aae16be7
4e1ik <Artemi324@tut.by>
2024-06-28 21:13:03 +0300

    fix products 20

--- resources/views/elitvid/site/pots/pot_product_page.blade.php
+++ resources/views/elitvid/site/pots/pot_product_page.blade.php
@@ -133,19 +133,7 @@
                     <!-- Additional required wrapper -->
                     <div class="offered_product-swiper-wrapper__slider swiper-wrapper">
                         <!-- Slides -->
-{{--                        @foreach($rand_products as $rand_product)--}}
-{{--                            <div class="swiper-slide">--}}
-{{--                                <div class="direction">--}}
-{{--                                    <img src="{{asset('storage/'.str_replace('public/','',$rand_product->pot_images[0]->image))}}"--}}
-{{--                                         alt="Фотография направления кашпо">--}}
-{{--                                    <a href="{{route('show_pot_product', ['id' => $rand_product->id])}}">--}}
-{{--                                        <button>{{$rand_product->name}}</button>--}}
-{{--                                    </a>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        @endforeach--}}
-
-                        @foreach($rand_products->where('collection', $product->collection)->get() as $rand_product)
+                        @foreach($rand_products as $rand_product)
                             <div class="swiper-slide">
                                 <div class="direction">
                                     <img src="{{asset('storage/'.str_replace('public/','',$rand_product->pot_images[0]->image))}}"
@@ -156,6 +144,18 @@
                                 </div>
                             </div>
                         @endforeach
+
+{{--                        @foreach($rand_products->where('collection', $product->collection)->get() as $rand_product)--}}
+{{--                            <div class="swiper-slide">--}}
+{{--                                <div class="direction">--}}
+{{--                                    <img src="{{asset('storage/'.str_replace('public/','',$rand_product->pot_images[0]->image))}}"--}}
+{{--                                         alt="Фотография направления кашпо">--}}
+{{--                                    <a href="{{route('show_pot_product', ['id' => $rand_product->id])}}">--}}
+{{--                                        <button>{{$rand_product->name}}</button>--}}
+{{--                                    </a>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        @endforeach--}}
                     </div>
 
                     <div class="swiper-button-prev arrow-left arrow">
