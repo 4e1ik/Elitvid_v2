b18cb8e1496bc22bf0c14a077c3e355d35abc17a
4e1ik <Artemi324@tut.by>
2024-06-28 20:45:35 +0300

    fix bench products 4

--- resources/views/elitvid/site/benches/bench_product_page.blade.php
+++ resources/views/elitvid/site/benches/bench_product_page.blade.php
@@ -210,8 +210,8 @@
                     <!-- Additional required wrapper -->
                     <div class="offered_product-swiper-wrapper__slider swiper-wrapper">
                         <!-- Slides -->
-                        @foreach($rand_products as $rand_product)
-                            @if($rand_product->collection == $product->collection)
+                        @foreach($rand_products->where('collection', $product->collection) as $rand_product)
+{{--                            @if($rand_product->collection == $product->collection)--}}
                             <div class="swiper-slide">
                                 <div class="direction">
                                     <img src="{{asset('storage/'.str_replace('public/','',$rand_product->bench_images[0]->image))}}"
@@ -221,7 +221,7 @@
                                     </a>
                                 </div>
                             </div>
-                            @endif
+{{--                            @endif--}}
                         @endforeach
                     </div>
 
