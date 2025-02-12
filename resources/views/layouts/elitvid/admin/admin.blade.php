ed804bef53de14d61a32b0de1e76dab8f61d9be6
artemi <Artemi324@tut.by>
2025-02-12 18:34:15 +0300

    init (12.02.2025): test

--- resources/views/layouts/elitvid/admin/admin.blade.php
+++ resources/views/layouts/elitvid/admin/admin.blade.php
@@ -658,8 +658,10 @@
                     </ul>
                 </li>
                 <li><a href="{{route('admin_metatags')}}"><span class="icons icon-tag"></span>Мета-теги</a></li>
+                <li><a href="{{route('admin_blog')}}"><span class="fa fa-newspaper-o"></span>Блог</a></li>
                 <li><a href="{{route('categories.index')}}"><span class="icons icon-list"></span>Описание категорий</a></li>
                 <li><a href="{{route('admin_blog')}}"><span class="fa fa-newspaper-o"></span>Блог</a></li>
+
             </ul>
         </div>
     </div>
