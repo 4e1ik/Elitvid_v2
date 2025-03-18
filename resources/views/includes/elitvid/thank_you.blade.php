dd90fab65be3701317b4bde67f722eb437317c0b
artemi <Artemi324@tut.by>
2025-03-19 00:54:25 +0300

    init (03.03.2025): fix thank_you

--- resources/views/includes/elitvid/thank_you.blade.php
+++ resources/views/includes/elitvid/thank_you.blade.php
@@ -2,8 +2,8 @@
 @section('content')
     <section class="stub">
         <div class="stub__text">
-            <h1>Спасибо за вашу заявку!</h1>
-            <p>Вы будете автоматически перенаправлены обратно через 2 секунды...</p>
+            <h3>Спасибо за вашу заявку!</h3>
+            <p>Вы будете автоматически перенаправлены обратно через 3 секунды...</p>
             <a href="{{ route('home') }}">Вернуться на главную</a>
         </div>
     </section>
@@ -11,6 +11,6 @@
         const referrer = new URLSearchParams(window.location.search).get('referrer') || '/';
         setTimeout(() => {
             window.location.href = referrer;
-        }, 2000);
+        }, 3000);
     </script>
 @endsection
