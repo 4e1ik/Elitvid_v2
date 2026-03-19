85d914f67ec78a343c1e5f5f03cc876471098ea9
Artemi Sevostian <asevostan@gmail.com>
2026-03-19 10:12:04 +0300

    fix: fix meta tags

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -21,12 +21,14 @@ class ViewServiceProvider extends ServiceProvider
     public function boot(): void
     {
         view()->composer('layouts.elitvid.elitvid', function ($view) {
+            $data = $view->getData();
             $view->with([
-                'metaTitle' => 'Изделия из полистоуна от производителя на заказ - Elitvid.com',
-                'metaDescription' => 'Качественные изделия из полистоуна от производителя Elitvid.com.
-                                        В нашем каталоге широкий выбор декоративных элементов для интерьера и экстерьера.
-                                        Надежность, эстетика и доступные цены – только у нас!',
-                'static_pages' => StaticPage::where('active', 1)->get(),
+                // Передаем дефолт только если контроллер не передал значение
+                'metaTitle' => $data['metaTitle'] ?? 'Изделия из полистоуна от производителя на заказ - Elitvid.com',
+                'metaDescription' => $data['metaDescription'] ?? 'Качественные изделия из полистоуна от производителя Elitvid.com.
+                                    В нашем каталоге широкий выбор декоративных элементов для интерьера и экстерьера.
+                                    Надежность, эстетика и доступные цены – только у нас!',
+                'static_pages' => $data['static_pages'] ?? StaticPage::where('active', 1)->get(),
             ]);
         });
     }
